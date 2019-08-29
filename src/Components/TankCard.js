a18981cb380f02dde4180d33bb7fe9ec853da8cc
Flatiron School <email.the.anderson@gmail.com>
2019-08-29 14:52:11 -0400

    basic search functionality added

--- src/Components/TankCard.js
+++ src/Components/TankCard.js
@@ -6,9 +6,9 @@ class TankCard extends Component {
     clickHandler = () => this.setState({ clicked: !this.state.clicked });
 
     render() {
-        console.log('tank card render firing')
+        // console.log('tank card render firing')
         return (
-            <div tankName = {this.props.tankInfo.name}>
+            <div name= {this.props.tankInfo.name}>
                 <center>
                 <h1 className="tank-name">{this.props.tankInfo.name}</h1>
                 <img onClick={this.clickHandler}src={this.props.tankInfo.img} alt={this.props.tankInfo.name} />
@@ -16,7 +16,7 @@ class TankCard extends Component {
                 {this.state.clicked ? <h3>Introduced: {this.props.tankInfo.year}</h3> : null}
                 {this.state.clicked ? <h3>Era: {this.props.tankInfo.era}</h3> : null}
                 {this.state.clicked ? <p className="strengths"> Strengths: {this.props.tankInfo.strengths}</p> : null}
-                {this.state.clicked ? <p className="weaknesses" >Weaknesses: {this.props.tankInfo.weaknesses}</p> : null}
+                {this.state.clicked ? <p className="weaknesses"> Weaknesses: {this.props.tankInfo.weaknesses}</p> : null}
                 </center>
                 
             </div>
--- src/Containers/TankContainer.js
+++ src/Containers/TankContainer.js
@@ -2,25 +2,49 @@ import React, { Component } from "react";
 import TankCard from "../Components/TankCard.js";
 
 class TankContainer extends Component {
-    state= { tanksArray: [],
+    state= { allTanksArray: [],
             ww2Array: [],
-            coldWarArray: []
+            coldWarArray: [],
+            selectedArray: []
     }
 
     componentDidMount(){
-        console.log('TankContainer mounted')
+        // console.log('TankContainer mounted')
         fetch('http://localhost:3000/tanks')
         .then(resp => resp.json())
-        .then(data => this.setState({tanksArray: data,
-                                    ww2Array: data.filter(tank => tank.era === 'World War II'),
-                                    coldWarArray: data.filter(tank => tank.era === 'Cold War')
-                                }))
+        .then(data => this.setState({
+            allTanksArray: data,
+            ww2Array: data.filter(tank => tank.era === 'World War II'),
+            coldWarArray: data.filter(tank => tank.era === 'Cold War'),
+            selectedArray: data
+        }))
+    }
+
+
+    changeEra = (era) => {
+        if(era === "World War II") {
+            this.setState({selectedArray: this.state.ww2Array})        
+        }
+        else if(era === 'Cold War') {
+            this.setState({selectedArray: this.state.coldWarArray})
+        }
+        else if(era ==='All') {
+            this.setState({selectedArray: this.state.allTanksArray})
+        }
+    }
+
+    searchTanks = event => {
+        if(event.target.value.length > 0) {
+        console.log(event.target.value)
+        this.setState({selectedArray: this.state.allTanksArray.filter(tank => tank.name.includes(event.target.value))})
+        }
+        else {this.setState({selectedArray: this.state.allTanksArray})}
     }
 
     render(){
-        console.log('tank container render firing')
-        console.log(this.state.tanksArray)
-        let tankComponents = this.state.tanksArray.map(
+        // console.log('tank container render firing')
+        // console.log(this.state.allTanksArray) 
+        let tankComponents = this.state.selectedArray.map(
             tank => <TankCard 
                 key={tank.name}
                 tankInfo={tank}
@@ -28,6 +52,14 @@ class TankContainer extends Component {
         )
         return(
             <div className="TanksContainer">
+                <center>
+                <button onClick={() => this.changeEra('World War II')}>World War II</button>
+                <button onClick={() => this.changeEra('Cold War')}>Cold War</button>
+                <button onClick={() => this.changeEra('All')}>All Eras</button>
+                </center>
+                <center>
+                <input type="text" name="tank-search" id="tank-search" onChange={this.searchTanks} value={this.state.messageText}/>
+                </center>
                 <ul>{tankComponents}</ul>
             </div>
         ) 
