4b88362e891bdb40eb3bb0246322916022aea93b
Flatiron School <email.the.anderson@gmail.com>
2019-08-29 12:51:37 -0400

    adjusted strength and weakness widths

--- src/Components/TankCard.js
+++ src/Components/TankCard.js
@@ -9,13 +9,16 @@ class TankCard extends Component {
         console.log('tank card render firing')
         return (
             <div tankName = {this.props.tankInfo.name}>
+                <center>
                 <h1 className="tank-name">{this.props.tankInfo.name}</h1>
                 <img onClick={this.clickHandler}src={this.props.tankInfo.img} alt={this.props.tankInfo.name} />
                 {this.state.clicked ? <h2>Country: {this.props.tankInfo.country}</h2> : null}
                 {this.state.clicked ? <h3>Introduced: {this.props.tankInfo.year}</h3> : null}
-                {this.state.clicked ? <h3>Introduced: {this.props.tankInfo.era}</h3> : null}
-                {this.state.clicked ? <p>Strengths: {this.props.tankInfo.strengths}</p> : null}
-                {this.state.clicked ? <p>Weaknesses: {this.props.tankInfo.weaknesses}</p> : null}
+                {this.state.clicked ? <h3>Era: {this.props.tankInfo.era}</h3> : null}
+                {this.state.clicked ? <p className="strengths"> Strengths: {this.props.tankInfo.strengths}</p> : null}
+                {this.state.clicked ? <p className="weaknesses" >Weaknesses: {this.props.tankInfo.weaknesses}</p> : null}
+                </center>
+                
             </div>
         )
     }
