772824a9f5f6e5339846883cd57ae346594ccef0
Flatiron School <email.the.anderson@gmail.com>
2019-08-29 13:28:54 -0400

    adding era filters

--- src/Containers/TankContainer.js
+++ src/Containers/TankContainer.js
@@ -2,17 +2,22 @@ import React, { Component } from "react";
 import TankCard from "../Components/TankCard.js";
 
 class TankContainer extends Component {
-    state= { tanksArray: [] }
+    state= { tanksArray: [],
+            ww2Array: [],
+            coldWarArray: []
+    }
 
     componentDidMount(){
         console.log('TankContainer mounted')
         fetch('http://localhost:3000/tanks')
         .then(resp => resp.json())
-        .then(data => this.setState({tanksArray: data}))
+        .then(data => this.setState({tanksArray: data,
+                                    ww2Array: data.filter(tank => tank.era === 'World War II'),
+                                    coldWarArray: data.filter(tank => tank.era === 'Cold War')
+                                }))
     }
 
     render(){
-        console.log('something something dark side')
         console.log('tank container render firing')
         console.log(this.state.tanksArray)
         let tankComponents = this.state.tanksArray.map(
